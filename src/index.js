12db27f1dc1a7954712def9a06b546201c4ca43e
Jordan Brown <jordan@Jordans-Air.attlocal.net>
2017-07-27 20:29:57 -0700

    embedded video workin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-const API_KEY = 'AIzaSyDxmnzEB5Xsn_Em4pz2EUxgpoPvQy3Yb_c'
+
+import YTSearch from 'youtube-api-search';
+
+// components Imported
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+
+const API_KEY = 'AIzaSyDxmnzEB5Xsn_Em4pz2EUxgpoPvQy3Yb_c'
+
+
 // Create new Component. This component should produce some HTML
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-            </div>
-    );
+class App  extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+            this.setState({ videos: data })
+            console.log(this.state);
+        });
+    }
+
+
+    render() {
+        return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos}/>
+        </div>
+        );
+    }
 }
 //Take the component generated HTML an put it on the page(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
